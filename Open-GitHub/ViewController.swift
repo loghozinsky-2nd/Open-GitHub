7e229bc181e91f863c44aac4f6b5ff7b17356db3
loghozinsky <loghozinsky@gmail.com>
2020-06-19 11:35:22 +0300

    Initial Commit

---
+++ Open-GitHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Open-GitHub
+//
+//  Created by Aleksander Logozinsky on 19.06.2020.
+//  Copyright © 2020 Aleksander Logozinsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
